bcfb6b4f2458681f07faa991c3983c299c13c62a
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-08 17:34:16 -0300

    Adding button with hover effects

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import styles from './Header.module.scss'
+
+export default function Header() {
+    return (
+        <header className={`d-flex flex-column justify-content-center align-items-center vw-100 vh-100`}>
+            <h1>Pedro Paulo Marcelino</h1>
+
+            <div className={styles.button_box}>
+                <button>
+                    <svg width="180px" height="60px" viewBox="0 0 180 60">
+                        <polyline points="179,1 179,59 1,59 1,1 179,1" />
+                        <polyline points="179,1 179,59 1,59 1,1 179,1" />
+                    </svg>
+                    
+                    <span className='fw-bold fs-5'>DETAILS</span>
+                </button>
+            </div>
+        </header>
+    )
+}
