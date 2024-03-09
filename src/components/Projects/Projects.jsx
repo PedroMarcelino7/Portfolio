b10ea38a01f8ae8355c5d4664b7532bf278cbe83
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-09 19:03:40 -0300

    Mapping all projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -19,50 +19,46 @@ export default function Projects() {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-lg-6 mb-5">
-                    <div className={`rounded-3 ${styles.project_card}`}>
-                        <img className='img-fluid rounded-top-3' onClick={() => setOpen(true)} src={slides[0].src} alt="" />
+                {slides.map((slide, key) => (
+                    <div className="col-lg-6 mb-5" key={key}>
+                        <div className={`rounded-3 ${styles.project_card}`}>
+                            <img className='img-fluid rounded-top-3 w-100' style={{ height: '350px' }} onClick={() => setOpen(true)} src={slide.src} alt="" />
 
-                        <div className={styles.project_description}>
-                            <h1>{slides[0].title}</h1>
+                            <div className={styles.project_description}>
+                                <h1>{slide.title}</h1>
 
-                            {/* MAP TECHNOLOGIES */}
-                            <div>
-                                {/* MAP */}
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
-                                <span className="badge text-bg-primary fs-6 m-1">Primary</span>
+                                <div>
+                                    {slide.technologies.map((technology, key) => (
+                                        <span className="badge text-bg-primary fs-6 m-1" key={key}>{technology}</span>
+                                    ))}
+                                </div>
+
+                                <div className="d-flex justify-content-end">
+                                    <button className='btn btn-outline-info fw-bold fs-6' onClick={() => setOpen(true)}>Description</button>
+                                </div>
                             </div>
 
-                            <div className="d-flex justify-content-end">
-                                <button className='btn btn-outline-info fw-bold fs-6' onClick={() => setOpen(true)}>Description</button>
-                            </div>
+                            <Lightbox
+                                plugins={[Captions, Zoom]}
+                                open={open}
+                                hideControls={true}
+                                render={{
+                                    buttonPrev: renderPrev ? undefined : () => null,
+                                    buttonNext: renderNext ? undefined : () => null,
+                                }}
+                                slides={[
+                                    {
+                                        src: slide.src,
+                                        title: slide.title,
+                                        description: slide.description,
+                                    }
+                                ]}
+
+                                close={() => setOpen(false)}
+                            />
                         </div>
-
-                        <Lightbox
-                            plugins={[Captions, Zoom]}
-                            open={open}
-                            hideControls={true}
-                            render={{
-                                buttonPrev: renderPrev ? undefined : () => null,
-                                buttonNext: renderNext ? undefined : () => null,
-                            }}
-                            slides={[
-                                {
-                                    src: slides[0].src,
-                                    title: slides[0].title,
-                                    description: slides[0].description,
-                                }
-                            ]}
-
-                            close={() => setOpen(false)}
-                        />
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     )
