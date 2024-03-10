e5cd6fe53a097702e78b5c13952356b4b826d776
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-10 16:35:54 -0300

    Adding About component + Border Effect >> https://web.dev/articles/css-border-animations

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,9 @@ export default function About() {
     return (
         <section id="about">
             <div className="container">
-
+                <div className={`${styles.project_border} p-4 w-100 rounded-3`}>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores dolore, natus suscipit ea similique porro doloremque quasi unde minus officia placeat aspernatur quas sint ducimus dolorem ut aut distinctio! Ex error cupiditate eaque optio aut sapiente eligendi veniam atque, saepe nihil at repudiandae amet dolores voluptas dolore necessitatibus, maxime molestias.</p>
+                </div>
             </div>
         </section>
     )
