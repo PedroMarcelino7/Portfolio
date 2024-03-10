f22fa3092ca5ae290cf7f792779ae2939346c272
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-10 19:43:22 -0300

    Adding technologies to slider

--- src/data.js
+++ src/data.js
@@ -31,24 +31,32 @@ export const projects = [
 
 import technology1 from './images/technologies/React.png'
 import technology2 from './images/technologies/Typescript.png'
+import technology3 from './images/technologies/Javascript.png'
+import technology4 from './images/technologies/MaterialUI.png'
+import technology5 from './images/technologies/HTML.png'
+import technology6 from './images/technologies/CSS.png'
+import technology7 from './images/technologies/Sass.png'
+import technology8 from './images/technologies/Bootstrap.png'
+import technology9 from './images/technologies/Java.png'
+import technology10 from './images/technologies/Python.png'
+import technology11 from './images/technologies/MySQL.png'
+import technology12 from './images/technologies/Vue.png'
+import technology13 from './images/technologies/PHP.png'
+import technology14 from './images/technologies/Git.png'
 
 export const technologies = [
-    {
-        image: technology1
-    },
-    {
-        image: technology2
-    },
-    // {
-    //     image: technology3
-    // },
-    // {
-    //     image: technology4
-    // },
-    // {
-    //     image: technology5
-    // },
-    // {
-    //     image: technology6
-    // },
+    { image: technology1 },
+    { image: technology2 },
+    { image: technology3 },
+    { image: technology4 },
+    { image: technology5 },
+    { image: technology6 },
+    { image: technology7 },
+    { image: technology8 },
+    { image: technology9 },
+    { image: technology10 },
+    { image: technology11 },
+    { image: technology12 },
+    { image: technology13 },
+    { image: technology14 },
 ]
