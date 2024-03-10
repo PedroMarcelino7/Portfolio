b63c75f820701128ef27b260f0e687ec980659a2
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-03-10 18:02:28 -0300

    Updating About Me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,18 @@ export default function About() {
         <section id="about">
             <div className="container">
                 <div className={`${styles.project_border} p-4 w-100 rounded-3`}>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores dolore, natus suscipit ea similique porro doloremque quasi unde minus officia placeat aspernatur quas sint ducimus dolorem ut aut distinctio! Ex error cupiditate eaque optio aut sapiente eligendi veniam atque, saepe nihil at repudiandae amet dolores voluptas dolore necessitatibus, maxime molestias.</p>
+                    <h1 className='fs-2'>ABOUT ME</h1>
+
+                    <hr />
+
+                    <p className='lead'>
+                        I am a student of <i>Systems Analysis and Development</i> at Fatec Guaratinguetá, 19 years old.
+                        I am deeply passionate about front-end development.
+                        Advanced English and proficient in a diverse range of programming languages, including
+                        <b> JavaScript</b>, <b>HTML5</b>, <b>CSS3</b>, <b>Java</b> and <b>Python</b>.
+                        Additionally, I am well-versed in utilizing frameworks and libraries, including
+                        <b> React</b>, <b>Typescript</b>, <b>Bootstrap</b>, <b>Material UI</b>, and <b>Sass</b>.
+                    </p>
                 </div>
             </div>
         </section>
