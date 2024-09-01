c86fff85125a8a8ef5cc119b5e7e9d2a91f1f1ff
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-01 17:45:22 -0300

    Updating about me layout with technologies carousel

--- src/components/Technologies/Technologies.jsx
+++ src/components/Technologies/Technologies.jsx
@@ -6,38 +6,18 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { technologies } from '../../data'
 
 export default function Technologies() {
-    const [slidePerView, setSlidePerView] = useState(1)
-
-    function handleResize() {
-        if (window.innerWidth < 768) {
-            setSlidePerView(1)
-        } else if (window.innerWidth < 992) {
-            setSlidePerView(2)
-        } else {
-            setSlidePerView(3)
-        }
-    }
-
-    useEffect(() => {
-        handleResize()
-        window.addEventListener('resize', handleResize)
-    }, [])
-
     return (
-        <section id='technologies' className={styles.technologies}>
-            <div className="container my-5">
-                <Swiper
-                    spaceBetween={0}
-                    slidesPerView={slidePerView}
-                    navigation
-                >
-                    {technologies.map((technology, index) => (
-                        <SwiperSlide key={index} className='d-flex justify-content-center align-items-center'>
-                            <img src={technology.image} alt="" style={{ width: '175px', height: '150px' }} />
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
+        <section id='technologies'>
+            <Swiper
+                slidesPerView={1}
+                navigation
+            >
+                {technologies.map((technology, index) => (
+                    <SwiperSlide key={index} className='d-flex justify-content-center align-items-center'>
+                        <img src={technology.image} alt="" style={{ width: '50%' }} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </section>
     )
 }
