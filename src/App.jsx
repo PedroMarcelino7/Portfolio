a1916cfe5ac1c9a5dc642127fdaccf918e85bd53
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-01 17:47:19 -0300

    Removing sections separator margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import './App.scss'
+
 import About from './components/About/About'
 import Certifications from './components/Certifications/Certifications'
 import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Projects from './components/Projects/Projects'
-import Technologies from './components/Technologies/Technologies'
 
 function App() {
   return (
-    <>
-      <div className='overflow'>
-        <Header />
+    <div className='overflow'>
+      <Header />
 
-        <About />
+      <About />
 
-        <Certifications />
+      <Certifications />
 
-        <Projects />
+      <Projects />
 
-        <div className='container d-flex flex-column'>
-          <span>Skills (technologies)</span>
-        </div>
-
-        <Contact />
-        
-        {/* <Technologies /> */}
-      </div>
-    </>
+      <Contact />
+    </div>
   )
 }
 
