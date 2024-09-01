b08b5d8a11f85209a765b3b98ca91b54478440d8
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-09-01 01:33:39 -0300

    Creating send e-mail function

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,52 @@
 import styles from './Contact.module.scss'
 
 const Contact = () => {
+    const accessKey = import.meta.env.VITE_EMAIL_ACCESS_KEY;
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+
+        formData.append("access_key", accessKey)
+
+        const email = formData.get('email')
+
+        const object = Object.fromEntries(formData)
+        const json = JSON.stringify(object)
+
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: json
+        }).then((res) => res.json())
+
+        if (res.success) {
+            alert('deu certo!')
+        }
+    }
+
     return (
         <div className={styles.contact}>
             <div className={styles.contact_box}>
-                <form action="">
-                    <input className={styles.input} placeholder='Name' />
+                <form onSubmit={(event) => onSubmit(event)}>
+                    <input className={styles.input} name='name' placeholder='Name' />
 
-                    <input className={styles.input} placeholder='E-mail' />
+                    <input className={styles.input} name='email' placeholder='E-mail' />
 
-                    <textarea className={styles.input} placeholder='Message'></textarea>
+                    <textarea className={styles.input} name='message' placeholder='Message'></textarea>
 
-                    <button type='submit' className={styles.button}>
-                        Submit
-                    </button>
+                    <div className={styles.submit_container}>
+                        <div className={styles.file_box}>
+                            <i class="bi bi-paperclip"></i>
+                        </div>
+
+                        <button type='submit' className={styles.button}>
+                            Submit
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>
